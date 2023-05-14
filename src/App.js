95a3b8e4b68ee3be97eed4daad697cb3631408c8
ShivaMisraa <shivam18557@gmail.com>
2023-05-14 16:12:48 +0530

    Added a loader

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-
+import Loader from "./components/Loader";
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   async function fetchMoviesHandler() {
+    setLoading (true)
     const response = await fetch("https://swapi.dev/api/films/");
     const data = await response.json();
 
@@ -19,6 +21,7 @@ function App() {
       };
     });
     setMovies(transformedMovies);
+    setLoading(false);
   }
 
   return (
@@ -27,7 +30,8 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+      {!isLoading && <MoviesList movies={movies} />}
+      {isLoading && <Loader/>}
       </section>
     </React.Fragment>
   );
